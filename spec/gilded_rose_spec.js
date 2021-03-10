e4ee5c058f806b5b64dc28fb22b39f7e91df65b4
PetarSimonovic <petar@simonovic.com>
2021-03-10 14:59:07 +0000

    Gilded Rose can add multiple items of stock

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,12 +1,18 @@
 //var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
 
-  describe("Stockkeeping", function() {
+  describe("initialisation", function() {
 
     it("can add an item of stock", function() {
       gildedRose = new Shop([ new Item("Aged Brie", 30, 50) ]);
-      items = gildedRose.updateQuality();
-      expect(items[0].name).toEqual("Aged Brie");
+      expect(gildedRose.items[0].name).toEqual("Aged Brie");
     });
+
+    it("can add multiple items of stock", function() {
+      gildedRose = new Shop([ new Item("Aged Brie", 20, 30), new Item("Backstage passes to a TAFKAL80ETC concert", 20, 30) ]);
+      expect(gildedRose.items[0].name).toEqual("Aged Brie");
+      expect(gildedRose.items[1].name).toEqual("Backstage passes to a TAFKAL80ETC concert");
+    });
+
   });
 });
