6fa28abb953acf8656e8040eb1757f398133b49d
PetarSimonovic <petar@simonovic.com>
2021-03-10 15:05:50 +0000

    tests refactored with specialItems variable

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,6 +1,12 @@
 //var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
 
+  let specialItems = [
+    new Item("Aged Brie", 30, 10),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 50, 5),
+    new Item("Sulfuras, Hand of Ragnaros", 0, 80)
+  ]
+
   describe("initialisation", function() {
 
     it("can add an item of stock", function() {
@@ -9,9 +15,10 @@ describe("Gilded Rose", function() {
     });
 
     it("can add multiple items of stock", function() {
-      gildedRose = new Shop([ new Item("Aged Brie", 20, 30), new Item("Backstage passes to a TAFKAL80ETC concert", 20, 30) ]);
+      gildedRose = new Shop(specialItems);
       expect(gildedRose.items[0].name).toEqual("Aged Brie");
       expect(gildedRose.items[1].name).toEqual("Backstage passes to a TAFKAL80ETC concert");
+      expect(gildedRose.items[2].name).toEqual("Sulfuras, Hand of Ragnaros");
     });
 
   });
